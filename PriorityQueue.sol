19efcebab0e1c35b1accf8621aee47b70dce9ac1
Amar Singh <ars9he@virginia.edu>
2018-02-12 23:22:11 -0500

    matic netwirk plasma implementation priorityqueue

---
+++ PriorityQueue.sol
@@ -0,0 +1,107 @@
+pragma solidity 0.4.18;
+
+
+//taken from Matic Network's Plasma Implementation. This works well.
+//https://github.com/maticnetwork/plasma/blob/master/contracts/ds/PriorityQueue.sol
+
+import "./auxiliary_contracts/SafeMath.sol";
+
+
+contract PriorityQueue {
+     using SafeMath for uint256;
+
+
+     //Modifiers
+     modifier onlyOwner() {
+           require(msg.sender == owner);
+           _;
+     }
+
+     //Storage
+     address owner;
+     uint256[] heapList;
+     uint256 public currentSize;
+
+     function PriorityQueue()
+          public
+    {
+         owner = msg.sender;
+         heapList = [0];
+         currentSize = 0;
+    }
+
+    function insert(uint256 k)
+         public
+         onlyOwner
+    {
+         heapList.push(k);
+         currentSize = currentSize.add(1);
+         percUp(currentSize);
+    }
+
+    function miniChild(uint256 i)
+         public
+         view
+         returns (uint256)
+    {
+         if (i.mul(2).add(1) > currentSize) {
+              return i.mul(2);
+         } else {
+              if (heapList[i.mul(2)] < heapList[i.mul(2).add(1)]) {
+                   return i.mul(2);
+              } else {
+                   return i.mul(2).add(1);
+              }
+         }
+    }
+
+    function getMin()
+         public
+         view
+         returns(uint256)
+    {
+         return heapList[1];
+    }
+
+    function delMin()
+         public
+         onlyOwner
+         returns (uint256)
+    {
+        uint256 retVal = heapList[1];
+        heapList[1] = heapList[currentSize];
+        delete heapList[currentSize];
+        currentSize = currentSize.sub(1);
+        percDown(1);
+        return retVal;
+    }
+
+    function percUp(uint256 s)
+        private
+    {
+         var i = s;
+         while (i.div(2) > 0) {
+               if (heapList[i] < heapList[i.div(2)]) {
+                    uint256 tmp = heapList[i.div(2)];
+                    heapList[i.div(2)] = heapList[i];
+                    heapList[i] = tmp;
+               }
+         i = i.div(2);
+         }
+    }
+
+    function percDown(uint256 s)
+         private
+    {
+         var i = s;
+         while (i.mul(2) <= currentSize) {
+              uint256 mc = miniChild(i);
+              if (heapList[i] > heapList[mc]) {
+                   uint256 tmp = heapList[i];
+                   heapList[i] = heapList[mc];
+                   heapList[mc] = tmp;
+              }
+         i = mc;
+         }
+    }
+}
